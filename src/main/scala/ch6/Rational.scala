fa155f73e277f328809dc005b5b3d1267d8e6e66
Thomas Swann <ts.swann@gmail.com>
2015-04-12 16:31:23 +0100

    CH6: Rational numbers in normalised form

--- src/main/scala/ch6/Rational.scala
+++ src/main/scala/ch6/Rational.scala
@@ -7,15 +7,31 @@ class Rational(n: Int, d: Int) {
 
   require(d != 0)
 
-  val numer: Int = n
-  val denom: Int = d
+  private val g = gcd(n.abs, d.abs)
+  val numer: Int = n / g
+  val denom: Int = d / g
+
+  // auxiliary constructor
+  def this(n: Int) = this(n, 1)
 
   override def toString = numer + "/" + denom
+
+  private def gcd(a: Int, b: Int): Int = {
+    if (b == 0) a else gcd(b, a % b)
+  }
 
   def add(that: Rational): Rational = {
     new Rational(
       numer * that.denom + that.numer * denom,
       denom * that.denom
     )
+  }
+
+  def lessThan(that: Rational) = {
+    this.numer * that.denom < that.numer * this.denom
+  }
+
+  def max(that: Rational) = {
+    if (this.lessThan(that)) that else this
   }
 }
