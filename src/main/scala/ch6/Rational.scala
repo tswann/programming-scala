0107fc326efe358347966d138558ecb7d353c9ed
Thomas Swann <ts.swann@gmail.com>
2015-04-12 16:37:09 +0100

    CH6: Added operators for Rational

--- src/main/scala/ch6/Rational.scala
+++ src/main/scala/ch6/Rational.scala
@@ -14,24 +14,20 @@ class Rational(n: Int, d: Int) {
   // auxiliary constructor
   def this(n: Int) = this(n, 1)
 
-  override def toString = numer + "/" + denom
-
-  private def gcd(a: Int, b: Int): Int = {
-    if (b == 0) a else gcd(b, a % b)
-  }
-
-  def add(that: Rational): Rational = {
+  def +(that: Rational): Rational = {
     new Rational(
       numer * that.denom + that.numer * denom,
       denom * that.denom
     )
   }
 
-  def lessThan(that: Rational) = {
-    this.numer * that.denom < that.numer * this.denom
+  def *(that: Rational): Rational = {
+    new Rational(numer * that.numer, denom * that.denom)
   }
 
-  def max(that: Rational) = {
-    if (this.lessThan(that)) that else this
+  override def toString = numer + "/" + denom
+
+  private def gcd(a: Int, b: Int): Int = {
+    if (b == 0) a else gcd(b, a % b)
   }
 }
