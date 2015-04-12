d1bbaef04989ee702487c1b10eeafac96622c501
Thomas Swann <ts.swann@gmail.com>
2015-04-12 16:19:38 +0100

    Ch6 Rational number example

---
+++ src/main/scala/ch6/Rational.scala
@@ -0,0 +1,21 @@
+package ch6
+/*
+ * Rational Number class -
+ * Example from Ch6 Programming in Scala
+ */
+class Rational(n: Int, d: Int) {
+
+  require(d != 0)
+
+  val numer: Int = n
+  val denom: Int = d
+
+  override def toString = numer + "/" + denom
+
+  def add(that: Rational): Rational = {
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom
+    )
+  }
+}
