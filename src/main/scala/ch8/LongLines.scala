e5d3bf1e84e43adc06c8f1f08afd8b4f1eb82009
Thomas Swann <ts.swann@gmail.com>
2015-04-13 19:57:17 +0100

    CH8: Refactored to use local function

--- src/main/scala/ch8/LongLines.scala
+++ src/main/scala/ch8/LongLines.scala
@@ -5,15 +5,15 @@ import scala.io.Source
 object LongLines {
 
   def processFile(filename: String, width: Int): Unit = {
+
+    def processLine(filename: String, width: Int, line: String): Unit = {
+      if (line.length > width)
+        println(filename + ": " + line.trim)
+    }
+
     val source = Source.fromFile(filename)
     for (line <- source.getLines()) {
       processLine(filename, width, line)
-    }
-  }
-
-  private def processLine(filename: String, width: Int, line: String): Unit = {
-    if (line.length > width) {
-      println(filename + ": " + line.trim)
     }
   }
 }
