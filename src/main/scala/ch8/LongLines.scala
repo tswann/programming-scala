cadb18a2d1adb95350587cb0e106fee30664d2a6
Thomas Swann <ts.swann@gmail.com>
2015-04-13 19:59:29 +0100

    CH8: Remove redundant local function params

--- src/main/scala/ch8/LongLines.scala
+++ src/main/scala/ch8/LongLines.scala
@@ -6,14 +6,14 @@ object LongLines {
 
   def processFile(filename: String, width: Int): Unit = {
 
-    def processLine(filename: String, width: Int, line: String): Unit = {
+    def processLine(line: String): Unit = {
       if (line.length > width)
         println(filename + ": " + line.trim)
     }
 
     val source = Source.fromFile(filename)
     for (line <- source.getLines()) {
-      processLine(filename, width, line)
+      processLine(line)
     }
   }
 }
