5cb41d651ba44fa9dfa6bd51762b390ebc7fc426
Thomas Swann <ts.swann@gmail.com>
2015-04-13 19:51:00 +0100

    CH8: Long line print example

---
+++ src/main/scala/ch8/LongLines.scala
@@ -0,0 +1,19 @@
+package ch8
+
+import scala.io.Source
+
+object LongLines {
+
+  def processFile(filename: String, width: Int): Unit = {
+    val source = Source.fromFile(filename)
+    for (line <- source.getLines()) {
+      processLine(filename, width, line)
+    }
+  }
+
+  private def processLine(filename: String, width: Int, line: String): Unit = {
+    if (line.length > width) {
+      println(filename + ": " + line.trim)
+    }
+  }
+}
